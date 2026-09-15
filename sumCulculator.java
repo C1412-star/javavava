a0735af6bce87aa7322f8fb970131be5d1db5b4a
C1412-star <2506322315@qq.com>
2026-09-15 11:41:05 +0800

    Add files via upload

---
+++ sumCulculator.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class sumCulculator {
+    public static void main(String[] args){
+        Scanner input =new Scanner(System.in);
+        System.out.print("the start number is:");
+        int start = input.nextInt();
+        System.out.print("the end number is:");
+        int end = input.nextInt();
+
+        if (start >= end){
+            System.out.print("error!Start number should be less than end number");
+        }else{int sum=0;
+            for(int counter = start ; counter<=end ; counter++){
+                sum = sum+counter;
+            }
+            System.out.println("the sum from " + start + " to " + end + " is:" +sum);
+
+        }
+
+
+    }
+}
